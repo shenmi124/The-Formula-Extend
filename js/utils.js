f4c8721e295ea8db13ff9625a28940614ff37e65
shenmi124 <67265011@qq.com>
2023-03-22 21:29:04 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -260,7 +260,7 @@ function updateMilestones(layer) {
 		if (!(hasMilestone(layer, id)) && layers[layer].milestones[id].done()) {
 			player[layer].milestones.push(id)
 			if (layers[layer].milestones[id].onComplete) layers[layer].milestones[id].onComplete()
-			if (tmp[layer].milestonePopups || tmp[layer].milestonePopups === undefined) doPopup("milestone", tmp[layer].milestones[id].requirementDescription, "Milestone Gotten!", 3, tmp[layer].color);
+			if (tmp[layer].milestonePopups || tmp[layer].milestonePopups === undefined) doPopup("milestone", options.ch ?  tmp[layer].milestones[id].requirementDescription : tmp[layer].milestones[id].requirementDescriptionEN, "Milestone Gotten!", 3, tmp[layer].color);
 			player[layer].lastMilestone = id
 		}
 	}
@@ -271,7 +271,7 @@ function updateAchievements(layer) {
 		if (isPlainObject(layers[layer].achievements[id]) && !(hasAchievement(layer, id)) && layers[layer].achievements[id].done() && tmp[layer].achievements[id].unlocked) {
 			player[layer].achievements.push(id)
 			if (layers[layer].achievements[id].onComplete) layers[layer].achievements[id].onComplete()
-			if (tmp[layer].achievementPopups || tmp[layer].achievementPopups === undefined) doPopup("achievement", tmp[layer].achievements[id].name, "Achievement Gotten!", 3, tmp[layer].color);
+			if (tmp[layer].achievementPopups || tmp[layer].achievementPopups === undefined) doPopup("achievement", options.ch ? tmp[layer].achievements[id].name : tmp[layer].achievements[id].nameEN, "Achievement Gotten!", 3, tmp[layer].color);
 		}
 	}
 }
